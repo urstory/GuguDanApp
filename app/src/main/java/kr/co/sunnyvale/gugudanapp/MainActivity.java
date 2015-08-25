56681310113b9b98f5e0d92f163694c089e7e95b
sungpark Kim <urstory@gmail.com>
2015-08-25 16:22:44 +0900

    추가

--- app/src/main/java/kr/co/sunnyvale/gugudanapp/MainActivity.java
+++ app/src/main/java/kr/co/sunnyvale/gugudanapp/MainActivity.java
@@ -1,9 +1,12 @@
 package kr.co.sunnyvale.gugudanapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,7 +14,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        final Button newGameBtn = (Button)findViewById(R.id.newGameBtn);
+        final Button scoreBtn = (Button)findViewById(R.id.scoreBtn);
+        final Button helpBtn = (Button)findViewById(R.id.helpBtn);
+
+        newGameBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        }); // newGameBtn 이 터치되면 onClick 부분이 실행된다.
+    } // onCreate
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
